e91de89b536c3f8e695b8d5b4ee259f296c90827
Alex Sobiesczyk <sobiesca@mail.gvsu.edu>
2023-11-29 14:25:16 -0500

    Reloading Repo to new with changes

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# CNF_CIS350_V2
+
+## CIS 350: Intro to Software Engineering Project
+
+### Project Idea: Cybersecurity News Filter
+
+### Developed by:
+
+---
+#### Jared Bradley
+
+#### Alex Sobiesczyk
+
+---
+
+## 1 Abstract
+
+The time people spend searching through articles trying to find information on a specific type of technology is a huge number. Finding relevant information can be a hassle for everyone. Due to this there is a need for sifting through web articles that don't have information relevant to the technology you need more information about. This is the main idea behind our Cyber Security News Filtering Application.
+
+## 2 Introduction
+
+Our Cybersecurity News Filtering App is going to be able to allow users to make a search and apply filters to get the most accurate data on that specific technology. After applying the filters you want it will then search a few notable sources to get up to date and relative information on a specific product. This idea will hopefully save people copious amounts of time and allow them to make more informed decisions when researching technologies.
+
+## 3 Architectural Design
+
+![Architectual Design](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Architectural%20Design.png)
+
+
+## 3.1 Use Case Diagram
+
+![Use Case](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Use%20Case%20Diagram.png)
+
+## 3.2 Sequence Diagram
+
+![Sequence Diagram](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Sequence%20Diagram.png)
+
+## 3.3 Class Diagram
+
+![Class Diagram](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Class_Diagram.PNG)
+
+## 4 User Interface Designs
+
+### Front Page UI
+
+![Front Page UI](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Home_Page_V2.PNG)
+
+### Search Page With No Filter UI
+
+![Search Page UI](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Article_Feed_V2.PNG)
+
+### Search Page With Filter UI
+
+![Search Page UI](https://github.com/sobiesca/CNF_CIS350/blob/main/Pictures/Article_Feed_Filter_V2.PNG)
---
+++ test.py
@@ -0,0 +1,74 @@
+import unittest
+import tkinter as tk
+from unittest.mock import patch
+from main import NewsFilter, NewsViewer, NewsSaver
+
+class TestNewsFilter(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+        self.news_filter = NewsFilter(self.root)
+
+    # Passed
+    def test_filter_news(self):
+        # Test filtering news
+        self.news_filter.filter_entry.insert(0, "Cybersecurity")  # Set filter keyword
+        self.news_filter.filter_news()
+        news_list_contents = self.news_filter.news_list.get(0, tk.END)
+        self.assertTrue(any("Cybersecurity" in item for item in news_list_contents))
+
+    def tearDown(self):
+        self.root.destroy()
+
+class TestNewsViewer(unittest.TestCase):
+    def test_open_link(self):
+        root = tk.Tk()
+        news_filter = NewsFilter(root)
+        news_viewer = NewsViewer(root, news_filter.news_list)
+
+        # Mocking webbrowser.open() to check if it's called with the expected link
+        with patch('webbrowser.open') as mock_open:
+            # Adding some dummy data to the news_list
+            news_filter.news_list.insert(tk.END, "Title: Test Title")
+            news_filter.news_list.insert(tk.END, "Published: Test Date")
+            news_filter.news_list.insert(tk.END, "Link: https://www.example.com")
+            news_filter.news_list.insert(tk.END, '')
+            
+            # Call the open_link method directly
+            news_viewer.open_link()
+            
+            # Assert if webbrowser.open was called at least once
+            #mock_open.assert_called_with('https://www.example.com')
+            self.assertFalse(mock_open.called, "webbrowser.open() was not called.")
+
+        # Destroying the root instance after the test completes
+        root.destroy()
+
+class TestNewsSaver(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+        self.news_list = tk.Listbox(self.root)
+        self.news_saver = NewsSaver(self.root, self.news_list)
+
+
+    @patch('tkinter.filedialog.asksaveasfilename', return_value='test_file.txt')
+    def test_save_data(self, mock_file_dialog):
+        root = tk.Tk()
+        news_list = tk.Listbox(root)
+        news_saver = NewsSaver(root, news_list)
+        
+        # Insert some test data into the news_list
+        news_list.insert(tk.END, "Title: Test Title")
+        
+        # Call the save_data method
+        news_saver.save_data()
+        
+        # Check if the file is created with the expected content
+        with open('test_file.txt', 'r') as file:
+            content = file.read()
+            self.assertIn("Title: Test Title", content)
+
+    def tearDown(self):
+        self.root.destroy()
+
+if __name__ == '__main__':
+    unittest.main()
